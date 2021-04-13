b78cf4e7b1009be9b7c9c6c0433052a72d1e4acb
iiy4383 <znlspf123@naver.com>
2021-04-13 18:01:21 +0900

    basically done

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,74 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    );
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Welcome there!');
+  describe('POST', () => {
+    it('/movies', () => {
+      const test = {
+        title: 'Testing on e2e',
+        year: 201,
+        genres: ['Please', 'pass', 'this'],
+      };
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send(test)
+        .expect(201);
+    });
+  });
+
+  describe('GET', () => {
+    it('/', () => {
+      return request(app.getHttpServer())
+        .get('/')
+        .expect(200)
+        .expect('Welcome there!');
+    });
+
+    it('/movies/1', () => {
+      return request(app.getHttpServer()).get('/movies/1').expect(200);
+    });
+
+    it('/movies/999', () => {
+      return request(app.getHttpServer()).get('/movies/999').expect(404);
+    });
+  });
+
+  describe('PATCH', () => {
+    it('/movies/1', () => {
+      const test = { year: 200 };
+      return request(app.getHttpServer())
+        .patch('/movies/1')
+        .send(test)
+        .expect(200);
+    });
+  });
+
+  describe('DELETE', () => {
+    it('/movies', () => {
+      return request(app.getHttpServer()).delete('/movies').expect(404);
+    });
+    it('/movies/1', () => {
+      return request(app.getHttpServer()).delete('/movies/1').expect(200);
+    });
   });
 });
